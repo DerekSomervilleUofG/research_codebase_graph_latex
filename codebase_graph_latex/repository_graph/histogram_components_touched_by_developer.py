a6b46b3bb4294862350e12d72d055456a347ba8c
Derek Somerville <derek.somerville@glasgow.ac.uk>
2025-11-23 11:02:25 +0000

    Histogram y-axis

--- codebase_graph_latex/repository_graph/histogram_components_touched_by_developer.py
+++ codebase_graph_latex/repository_graph/histogram_components_touched_by_developer.py
@@ -7,7 +7,7 @@ FILE_NAME = __name__
 BASE_FILE_NAME = "repository_summary_1.tex"
 GRAPH_CAPTION = "By {freq} for {category} {number} developers."
 MAX_X_AXIS = 4
-MAX_Y_AXIS = 50
+MAX_Y_AXIS = {"All": 1250, TRANSIENT_FOUNDER: 20, MODERATE_FOUNDER: 20, SUSTAINED_FOUNDER: 20, TRANSIENT_JOINER: 1000, MODERATE_JOINER: 250, SUSTAINED_JOINER: 100}
 
 FIGURE_CAPTION = "Histogram of new {component} touched (x-axis) against number of developers (y-axis) from " + word_engine.number_to_words(len(DEVELOPER_CATEGORY)) + " " + str(len(DEVELOPER_CATEGORY)) + " categories of developers from {number_of_repositories} sampled from GitHub." 
 
@@ -21,8 +21,8 @@ def get_commit_and_daily(developers):
     return commit_data, daily_data
 
 def generate_summary_histogram(commit_data, daily_data, component, category):
-    latex_repo = developer_graph(FILE_NAME, daily_data, component, 0, category + "Day", "", GRAPH_CAPTION.format(freq="Day", number=str(len(daily_data)), category=category), max_x_axis=max(max(daily_data),max(commit_data)) + 1, max_y_axis=MAX_Y_AXIS) 
-    latex_repo += developer_graph(FILE_NAME, commit_data, component, 0, category + "Commit", "", GRAPH_CAPTION.format(freq="Commit", number=str(len(commit_data)), category=category), max_x_axis=max(max(daily_data),max(commit_data)) + 1, max_y_axis=MAX_Y_AXIS) 
+    latex_repo = developer_graph(FILE_NAME, daily_data, component, 0, category + "Day", "", GRAPH_CAPTION.format(freq="Day", number=str(len(daily_data)), category=category), max_x_axis=max(max(daily_data),max(commit_data)) + 1, max_y_axis=MAX_Y_AXIS[category]) 
+    latex_repo += developer_graph(FILE_NAME, commit_data, component, 0, category + "Commit", "", GRAPH_CAPTION.format(freq="Commit", number=str(len(commit_data)), category=category), max_x_axis=max(max(daily_data),max(commit_data)) + 1, max_y_axis=MAX_Y_AXIS[category]) 
     return latex_repo
 
 def generate_component_summary_historgram(component, number_of_repositories):
