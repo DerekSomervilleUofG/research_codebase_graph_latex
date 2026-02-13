b7eba2a36b0be5bab252da0c8b60cf8c0fa72e2e
Derek Somerville <derek.somerville@glasgow.ac.uk>
2026-02-13 16:12:25 +0000

    get data for first year only

--- scratch.py
+++ scratch.py
@@ -1,12 +1,16 @@
-import math
-
-def round_to_x_minus_1_digits_nearest_5(num):
-    x = len(str(abs(int(num)))) if num != 0 else 1  # ensure x≥1 even for 0
-    step = 5 * 10**(x - 2)
-    return math.ceil(num / step) * step
-
-
-print(round_to_x_minus_1_digits_nearest_5(4))
-print(round_to_x_minus_1_digits_nearest_5(147))
-
-print(round_to_x_minus_1_digits_nearest_5(1147))
+def bubble_sort(numbers):
+    is_sorted = False
+    counter = 1
+    index = len(numbers) - 1
+    inner_index = 0
+    while index >= 1 and not is_sorted:
+        is_sorted = True
+        for inner_index in range(index):
+            if numbers[inner_index] > numbers[inner_index + 1]:
+                numbers[inner_index], numbers[inner_index + 1] = \
+                    numbers[inner_index + 1], numbers[inner_index]
+                is_sorted = False
+        counter += 1
+    print(counter)
+    return numbers
+print(bubble_sort([7,3,5,2,4]))
